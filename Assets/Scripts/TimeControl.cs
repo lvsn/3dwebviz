f13a8674ff67d99e45f399bab98270f7396460a4
MathGaron <mathieugaron91@gmail.com>
2016-04-11 20:49:45 -0400

    control the sun with Q and E

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TimeControl : MonoBehaviour {
+public class TimeControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    public float speed = 0.1f;
+    private enum lightState { Auto = 0, Man = -1 };
+    private lightState state = lightState.Auto;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(new Vector3(0, 0.1F, 0));
-	}
+	void Update ()
+    {
+        float rotation = 0;
+        if( state == lightState.Auto)
+        {
+            rotation = speed;
+        }
+        else if(state == lightState.Man)
+        {
+            if(Input.GetKey(KeyCode.Q))
+            {
+                rotation = -speed * 10;
+            }
+            if(Input.GetKey(KeyCode.E))
+            {
+                rotation = speed * 10;
+            }
+        }
+        transform.Rotate(new Vector3(0, rotation, 0));
+    }
+
+    void OnGUI()
+    {
+        uint buttonWidth = 80;
+        uint buttonHeight = 30;
+        uint spacing = 20;
+        Rect ButtonRect = new Rect(Screen.width - buttonWidth - spacing, Screen.height - buttonHeight - spacing, buttonWidth, buttonHeight);
+        if (GUI.Button(ButtonRect, "Light:" + state.ToString()))
+        {
+            state = ~state;
+        }
+    }
 }
