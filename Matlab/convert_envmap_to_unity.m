ed65e44ab4bcc4097379e963b413d812a932d67e
Yannick Hold-Geoffroy <yannick.hold-geoffroy.1@ulaval.ca>
2016-04-27 02:04:20 -0400

    Fixed generation of skybox

--- Matlab/convert_envmap_to_unity.m
+++ Matlab/convert_envmap_to_unity.m
@@ -1,6 +1,6 @@
 function [] = convert_envmap_to_unity(filename)
 e = EnvironmentMap(filename);
-ecube = e.convertTo('cube');
+ecube = e.convertTo('cube', 4096);
 
 %% Tonemapping
 ec = 1 * ecube.data ./ (1 + ecube.data);
@@ -16,10 +16,10 @@ ec = max(0, min(1, ec));
 ec = uint8(ec*255);
 
 imwrite(ec(round(height/4):round(2*height/4),round(width/3):round(2*width/3),:), 'forward.png');
-imwrite(ec(round(3*height/4):end,round(width/3):round(2*width/3),:), 'backward.png');
-imwrite(ec(round(height/4):round(2*height/4),1:round(width/3),:), 'left.png');
-imwrite(ec(round(height/4):round(2*height/4),round(2*width/3):end,:), 'right.png');
-imwrite(ec(1:round(height/4),round(width/3):round(2*width/3),:), 'top.png');
+imwrite(ec(end:-1:round(3*height/4),round(2*width/3):-1:round(width/3)+1,:), 'backward.png');
+imwrite(ec(round(height/4)+1:round(2*height/4),1:round(width/3),:), 'left.png');
+imwrite(ec(round(height/4)+1:round(2*height/4),round(2*width/3):end,:), 'right.png');
+imwrite(ec(1:round(height/4),round(width/3)+1:round(2*width/3),:), 'top.png');
 imwrite(ec(round(2*height/4):round(3*height/4),round(width/3):round(2*width/3),:), 'down.png');
 
 end
